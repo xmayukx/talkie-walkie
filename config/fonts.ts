4d09a33d56e1e8c7a687690b56bd326d61e37888
Mayukh Hazari <hazari.mayukh77@gmail.com>
2023-09-02 18:38:06 +0530

    Add initial files

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,21 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
+import {
+  Fira_Code as FontMono,
+  Inter as FontSans,
+  Exo_2 as Exo,
+  Titillium_Web as Teli,
+} from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
+
+export const exo = Exo({
+  subsets: ["latin"],
+  variable: "--font-exo",
+});
